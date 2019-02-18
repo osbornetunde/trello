f76816aa2a9a82d518c4c1e802d23e4b8ce7efbb
Tunde Osborne <osbornetunde@gmail.com>
2019-02-19 00:58:28 +0100

    Updates

---
+++ precache-manifest.b1f33f5185f11b71f442f1df86d06cc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c753dc64c993055f2672",
+    "url": "/trello/static/js/runtime~main.c753dc64.js"
+  },
+  {
+    "revision": "835a38b9b0aeeb2be841",
+    "url": "/trello/static/js/main.835a38b9.chunk.js"
+  },
+  {
+    "revision": "945ac6105096091d7649",
+    "url": "/trello/static/js/1.945ac610.chunk.js"
+  },
+  {
+    "revision": "835a38b9b0aeeb2be841",
+    "url": "/trello/static/css/main.060aba06.chunk.css"
+  },
+  {
+    "revision": "68e0b6112c5feb679ec56235471009e5",
+    "url": "/trello/index.html"
+  }
+];
